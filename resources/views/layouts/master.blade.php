dbdde9caf8f7c9802fa724213d3d2cb3596ebf36
Richard nwonah <nwonahr@gmail.com>
2021-08-22 00:01:06 +0100

    added laravel project and edited welcome page

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+    integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+<link href='http://fonts.googleapis.com/css?family=Arizonia' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" href="{{ URL::to('css/styles.css') }}">
+</head>
+<body>
+    @include('partials.header')
+    @yield('content')
+</body>
+</html>
